ed4e1839b80901f20154ca196ae5789d5919ab88
adam-omah <34926918+adam-omah@users.noreply.github.com>
2025-02-03 13:56:29 +0000

    Updates to Tables and main, to use in memory cache as table store, test updated to reflect this change.

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,34 @@
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
-use std::fs::File;
-use std::io::{BufRead, BufReader, Result};
+use actix_web::{web, App, HttpServer};
+use std::collections::BTreeMap;
+use std::io::{Result};
 use std::string::String;
 use std::sync::{Arc, Mutex};
 
+// Module Imports.
 pub mod config;
 pub mod schema;
-pub mod records; // Use 'table' module name
+pub mod records;
 
 use crate::config::database_config::DatabaseConfig;
+
+use crate::records::table::get_table;
 use schema::{
     schema::load_schema,
     schema::Schema,
 };
 
-pub const DB_DIR: &str = "mydb"; // Make DB_DIR public
+// public constants
+pub const DB_DIR: &str = "mydb";
 pub const SCHEMA_FILE: &str = "schema.json";
-pub const TABLE_DIR: &str = "tables"; // Added TABLE_DIR constant
+pub const TABLE_DIR: &str = "tables";
 
-
+#[derive(Clone)]
 pub struct AppState {
     pub schema: Arc<Mutex<Schema>>,
     pub config: DatabaseConfig,
+    pub cache: Arc<Mutex<BTreeMap<String, Vec<Vec<String>>>>>
 }
+
 
 fn init_database(config: &DatabaseConfig) -> Result<()> {
     std::fs::create_dir_all(&config.db_dir)?;
@@ -35,13 +41,13 @@ async fn main() -> Result<()> {
     let config = DatabaseConfig::default();
     init_database(&config)?;
     let schema = Arc::new(Mutex::new(load_schema(&config)?));
+    let cache = Arc::new(Mutex::new(BTreeMap::new()));
 
     HttpServer::new(move || {
         App::new()
-            .app_data(web::Data::new(AppState {
-                schema: schema.clone(),
+            .app_data(web::Data::new(AppState { schema: schema.clone(),
                 config: config.clone(),
-            }))
+                cache: cache.clone(),}))
             .service(get_table)
     })
         .bind(("127.0.0.1", 8080))?
@@ -51,42 +57,7 @@ async fn main() -> Result<()> {
 }
 
 
-#[get("/tables/{table_name}")]
-async fn get_table(path: web::Path<String>, data: web::Data<AppState>) -> impl Responder {
-    let table_name = path.into_inner();
-    let state = data.into_inner();
-    let schema = state.schema.lock().unwrap();
 
-    if let Some(_table) = schema.tables.get(&table_name) {
-        let table_path = state.config.db_dir.join(state.config.table_dir.as_path()).join(&table_name);
-        if let Ok(file) = File::open(table_path) {
-            let reader = BufReader::new(file);
-            let mut table_data = Vec::new();
-
-            for line_result in reader.lines() {
-                if let Ok(line) = line_result {
-                    let row_values: Vec<String> = line
-                        .split(',')
-                        .map(|s| s.trim_matches('"').to_string()) // Remove quotes during parsing
-                        .collect();
-                    table_data.push(row_values)
-                } else {
-                    return HttpResponse::InternalServerError().body("Error reading a line");
-                }
-            }
-
-            // Serialize table data to JSON
-            match serde_json::to_string(&table_data) {
-                Ok(json) => HttpResponse::Ok().body(json),
-                Err(e) => HttpResponse::InternalServerError().body(format!("Serialization error: {}", e)),
-            }
-        } else {
-            HttpResponse::NotFound().body("Table data file not found")
-        }
-    } else {
-        HttpResponse::NotFound().body("Table not found in schema")
-    }
-}
 
 
 /*
@@ -97,7 +68,7 @@ async fn get_table(path: web::Path<String>, data: web::Data<AppState>) -> impl R
 async fn test_application() -> Result<()> {
     use std::fs;
     use actix_web::http::StatusCode;
-    use actix_web::{test};
+    use actix_web::test;
     use serde_json::Value;
     use std::path::PathBuf;
     use schema::{schema::load_schema, schema::Column, schema::Table};
@@ -120,6 +91,17 @@ async fn test_application() -> Result<()> {
 
     init_database(&test_config)?;
     let schema = Arc::new(Mutex::new(load_schema(&test_config)?));
+    let cache = Arc::new(Mutex::new(BTreeMap::new()));
+
+
+    // Create a test application with your route
+    let app_state = web::Data::new(AppState {
+        schema: schema.clone(),
+        config: test_config.clone(),
+        cache: cache.clone(),
+    });
+
+
 
     if !schema.lock().unwrap().tables.contains_key("users") {
         let user_table = Table {
@@ -127,34 +109,32 @@ async fn test_application() -> Result<()> {
             columns: vec![
                 Column {
                     name: "id".to_string(),
-                    data_type: crate::schema::schema::DataType::Int,
+                    data_type: schema::schema::DataType::Int,
                     rules: vec![],
                 },
                 Column {
                     name: "name".to_string(),
-                    data_type: crate::schema::schema::DataType::String,
+                    data_type: schema::schema::DataType::String,
                     rules: vec![],
                 },
             ],
         };
-
-        create_table(&user_table, &test_config)?;
-        insert_row("users", vec!["1".to_string(), "Alice".to_string()], &test_config)?;
-        insert_row("users", vec!["2".to_string(), "Bob".to_string()], &test_config)?; // Correct insert_row usage
+        if !schema.lock().unwrap().tables.contains_key("users") {
+            create_table(&user_table, &app_state)?;
+            insert_row("users", vec!["1".to_string(), "Alice".to_string()], &app_state)?;
+            insert_row("users", vec!["2".to_string(), "Bob".to_string()], &app_state)?;
+        };
     };
 
-    let schema = Arc::new(Mutex::new(load_schema(&test_config)?));
 
-    // Create a test application with your route
+
     let app = test::init_service(
         App::new()
-            .app_data(web::Data::new(AppState {
-                schema: schema.clone(), // Use schema.clone() in app_data
-                config: test_config.clone(),
-            }))
+            .app_data(app_state.clone())
             .service(get_table),
     )
         .await;
+
 
     // Create a test request
     let req = test::TestRequest::get().uri("/tables/users").to_request();
--- src/records/table.rs
+++ src/records/table.rs
@@ -1,80 +1,168 @@
-use std::fs::{File, OpenOptions};
-use std::io::{Result, Write};
-use std::path::Path;
-
-use crate::schema::{schema::create_table as schema_create_table
-                    , schema::load_schema,
-                    schema::DataType,
-                    schema::Table};
 // Import necessary items
 use crate::schema::schema::check_column_rules;
-use crate::config::database_config::DatabaseConfig;
+use crate::schema::{schema::create_table as schema_create_table
+                    ,
+                    schema::DataType,
+                    schema::Table};
+use crate::AppState;
+use actix_web::{get, web, HttpResponse, Responder};
+use std::fs::{metadata, File, OpenOptions};
+use std::io::{BufRead, BufReader, Result, Write};
+use std::path::Path;
 
-pub fn create_table(table: &Table, config: &DatabaseConfig) -> Result<()> { // Add config parameter
-    let mut schema = load_schema(config)?;
-    schema_create_table(&mut schema, table.clone(), config)?;
+pub fn create_table(table: &Table, state: &web::Data<AppState>) -> Result<()> {
+    let mut schema = state.schema.lock().unwrap();
+    schema_create_table(&mut schema, table.clone(), &state.config)?;
+    drop(schema); // Release the lock on schema early
 
-    let table_dir = config.db_dir.join(config.table_dir.as_path()); // Use config for path
+    let table_dir = state.config.db_dir.join(state.config.table_dir.as_path());
     std::fs::create_dir_all(&table_dir)?;
 
     let table_path = table_dir.join(&table.name);
     File::create(table_path)?;
+
+    let mut cache = state.cache.lock().unwrap();
+    cache.insert(table.name.clone(), Vec::new()); // Initialize cache for new table
     Ok(())
 }
 
+pub fn insert_row(table_name: &str, row_data: Vec<String>, state: &web::Data<AppState>) -> Result<()> {
+    let mut cache = state.cache.lock().unwrap();
+    let column_types: Vec<DataType>;
 
-pub fn insert_row(table_name: &str, row_data: Vec<String>, config: &DatabaseConfig) -> Result<()> { // Add config parameter
-    let schema = load_schema(config)?;
-    let table = schema.tables.get(table_name).ok_or(std::io::Error::new(
-        std::io::ErrorKind::NotFound,
-        "Table not found",
-    ))?;
-
-    if row_data.len() != table.columns.len() {
-        return Err(std::io::Error::new(
-            std::io::ErrorKind::InvalidInput,
-            "Data length mismatch",
-        ));
-    }
-
-    let table_dir = Path::new(config.db_dir.as_path()).join(config.table_dir.as_path());
-    let table_path = table_dir.join(table_name);
-
-    let mut file = OpenOptions::new().append(true).create(true).open(table_path)?;
-    let mut serialized_row = String::new();
-
-    for (i, value) in row_data.iter().enumerate() {
-        let column = &table.columns[i];
-        let checked_value = check_column_rules(column, value)?; // Call check_column_rules HERE
-
-        // Handle the Option<String> returned by check_column_rules
-        match checked_value {
-            Some(final_value) => {
-                // Use final_value for serialization if it isn't Null
-                match column.data_type {
-                    DataType::Int => {
-                        match final_value.parse::<i64>() { // Parse the string into a valid integer
-                            Ok(int_val) => serialized_row.push_str(&format!("{}", int_val)),
-                            Err(_) => {
-                                return Err(std::io::Error::new(
-                                    std::io::ErrorKind::InvalidData,
-                                    "Invalid integer",
-                                ))
-                            }
-                        }
-                    }
-                    DataType::String => serialized_row.push_str(&format!("\"{}\"", final_value)),
-                }
+    // 1. Check Cache First
+    if let Some(cached_table) = cache.get_mut(table_name) {
+        // Lock schema to get column types
+        {
+            let schema = state.schema.lock().unwrap();
+            let table = schema.tables.get(table_name).expect("Table not found in schema, even though it's in the cache. This is a serious internal error.");
+            if row_data.len() != table.columns.len() {
+                return Err(std::io::Error::new(std::io::ErrorKind::InvalidInput, "Data length mismatch"));
             }
-            None => {
-                serialized_row.push_str("NULL");
+            column_types = table.columns.iter().map(|c| c.data_type.clone()).collect();
+        } // schema lock dropped here
+
+        // Process cached table using pre-loaded column_types
+        let mut serialized_row = String::new();
+        for (i, value) in row_data.iter().enumerate() {
+            let checked_value = check_column_rules(&state.schema.lock().unwrap().tables[table_name].columns[i], value)?;
+
+            match checked_value {
+                Some(final_value) => {
+                    if column_types[i] == DataType::Int { // Access column_types
+                        match final_value.parse::<i64>() {
+                            Ok(int_val) => serialized_row.push_str(&format!("{}", int_val)),
+                            Err(_) => return Err(std::io::Error::new(std::io::ErrorKind::InvalidData, "Invalid integer")),
+                        }
+                    } else if column_types[i] == DataType::String { // Access column_types
+                        serialized_row.push_str(&format!("\"{}\"", final_value));
+                    }
+                },
+                None => serialized_row.push_str("NULL"),
+            }
+            if i < row_data.len() - 1 {
+                serialized_row.push_str(",");
             }
         }
+        cached_table.push(row_data.clone());
+    } else {
+        // 2. Check if File Exists on Filesystem (New Step)
+        let table_dir = Path::new(state.config.db_dir.as_path()).join(state.config.table_dir.as_path());
+        let table_path = table_dir.join(table_name);
 
-        if i < row_data.len() - 1 {
-            serialized_row.push_str(",");
+        if metadata(&table_path).is_ok() {
+            // File exists, but not in cache. Load from file system later and insert into cache.
+            cache.insert(table_name.to_string(), Vec::new()); // Initialize the entry with an empty vector
+        } else {
+            // 3. If File Doesn't Exist, Check Schema
+            let schema = state.schema.lock().unwrap();
+
+            let table = schema.tables.get(table_name).ok_or(std::io::Error::new(
+                std::io::ErrorKind::NotFound,
+                "Table not found in schema or filesystem", // Updated error message
+            ))?;
+
+            if row_data.len() != table.columns.len() {
+                return Err(std::io::Error::new(
+                    std::io::ErrorKind::InvalidInput,
+                    "Data length mismatch",
+                ));
+            }
+
+            drop(schema);
+            cache.insert(table_name.to_string(), vec![row_data.clone()]); // Add to cache
+        }
+    }
+
+    // Write to File (All cases write to the file, but using the correct information source)
+    let table_dir = Path::new(state.config.db_dir.as_path()).join(state.config.table_dir.as_path());
+    let table_path = table_dir.join(table_name);
+    let mut file = OpenOptions::new().append(true).create(true).open(table_path)?;
+
+    // Serialization is the same regardless of the source
+    let row_to_serialize = &cache[table_name].last().unwrap();
+    let mut serialized_row = String::new();
+    for (i, value) in row_to_serialize.iter().enumerate() {
+        if i > 0 {
+            serialized_row.push(',');
+        }
+        if let DataType::String = state.schema.lock().unwrap().tables[table_name].columns[i].data_type {
+            serialized_row.push('"');
+            serialized_row.push_str(value);
+            serialized_row.push('"');
+        } else {
+            serialized_row.push_str(value);
         }
     }
     writeln!(file, "{}", serialized_row)?;
     Ok(())
+}
+
+// API end point for get tables.
+
+#[get("/tables/{table_name}")]
+async fn get_table(path: web::Path<String>, data: web::Data<AppState>) -> impl Responder {
+    let table_name = path.into_inner();
+    let state = data.into_inner();
+    let mut cache = state.cache.lock().unwrap();
+
+    if let Some(table_data) = cache.get(&table_name) {
+        match serde_json::to_string(&table_data) {
+            Ok(json) => return HttpResponse::Ok().body(json),
+            Err(e) => return HttpResponse::InternalServerError().body(format!("Serialization error: {}", e)),
+        }
+    }
+
+    // Case for where Table is not found in the cache.
+    let schema = state.schema.lock().unwrap();
+    if let Some(_table) = schema.tables.get(&table_name) {
+        let table_path = state.config.db_dir.join(state.config.table_dir.as_path()).join(&table_name);
+        if let Ok(file) = File::open(table_path) {
+            let reader = BufReader::new(file);
+            let mut table_data = Vec::new();
+
+            for line_result in reader.lines() {
+                if let Ok(line) = line_result {
+                    let row_values: Vec<String> = line
+                        .split(',')
+                        .map(|s| s.trim_matches('"').to_string())
+                        .collect();
+                    table_data.push(row_values)
+                } else {
+                    return HttpResponse::InternalServerError().body("Error reading a line");
+                }
+            }
+
+            cache.insert(table_name.clone(), table_data.clone());
+
+            match serde_json::to_string(&table_data) {
+                Ok(json) => HttpResponse::Ok().body(json),
+                Err(e) => HttpResponse::InternalServerError().body(format!("Serialization error: {}", e)),
+            }
+        } else {
+            HttpResponse::NotFound().body("Table data file not found")
+        }
+    } else {
+        HttpResponse::NotFound().body("Table not found in schema")
+    }
 }
